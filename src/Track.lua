553c5c41bd4cbdff0081950096529c0431dc7290
Alex Ruger <alexrugermusic@gmail.com>
2023-11-12 00:32:37 -0600

    added media items fun

--- src/Track.lua
+++ src/Track.lua
@@ -4,6 +4,7 @@
 dofile(reaper.GetResourcePath() .. "/Scripts/rewgs-reaper-scripts/modules/Naming.lua")
 
 Track = {
+    index = i,
 }
 
 -- local _media_track = reaper.GetTrack(0, i)
@@ -26,21 +27,6 @@ function Track:parent()
     self.parent = reaper.GetMediaTrackInfo_Value(self.media_track, "P_PARTRACK")
 end
 
--- local num_media_items = reaper.CountTrackMediaItems(media_track)
-function Track:num_media_items()
-    self.num_media_items = reaper.CountTrackMediaItems(self.media_track)
-end
-
--- local is_selected = reaper.IsTrackSelected(media_track)
-function Track:selection_state()
-    self.selection_state = reaper.IsTrackSelected(self.media_track)
-end
-
--- local track_mute_state = reaper.GetMediaTrackInfo_Value(media_track, "B_MUTE")
-function Track.mute_state()
-    reaper.GetMediaTrackInfo_Value(self.media_track, "B_MUTE")
-end
-
 function Track:parent_name(reaper_project)
     for i = 0, reaper.CountTracks(reaper_project) - 1 do
         local media_track = reaper.GetTrack(reaper_project)
@@ -55,6 +41,31 @@ function Track:parent_name(reaper_project)
     end
 end
 
+-- local num_media_items = reaper.CountTrackMediaItems(media_track)
+function Track:num_media_items()
+    self.num_media_items = reaper.CountTrackMediaItems(self.media_track)
+end
+
+function Track:media_items(reaper_project)
+    local track_media_items = {}
+    for i = 0, self.num_media_items - 1 do
+        local media_item = reaper.GetMediaItem(reaper_project, i)
+        local mute_state = reaper.GetMediaItemInfo_Value(media_item, "B_MUTE_ACTUAL")
+        table.insert(track_media_items, {media_item, mute_state})
+    end
+    self.media_items = track_media_items
+end
+
+-- local is_selected = reaper.IsTrackSelected(media_track)
+function Track:selection_state()
+    self.selection_state = reaper.IsTrackSelected(self.media_track)
+end
+
+-- local track_mute_state = reaper.GetMediaTrackInfo_Value(media_track, "B_MUTE")
+function Track.mute_state()
+    reaper.GetMediaTrackInfo_Value(self.media_track, "B_MUTE")
+end
+
 function Track:is_marked(track_marks)
     local marks = {}
     for _, mark in ipairs(track_marks) do
@@ -67,6 +78,7 @@ function Track:is_marked(track_marks)
         self.marks = marks
     end
 end
+
 
 -- TODO
 -- function Track:children(reaper_project)
