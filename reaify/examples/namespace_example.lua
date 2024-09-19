922c0203a81236fa87cc80b7046ee5625909053a
Alex Ruger <alexrugermusic@gmail.com>
2024-09-19 11:44:45 -0700

    Minor cleanup.

--- reaify/examples/namespace_example.lua
+++ reaify/examples/namespace_example.lua
@@ -1,3 +1,7 @@
+-- Some good info:
+-- - https://stackoverflow.com/questions/4394303/how-to-make-namespace-in-lua
+-- - https://www.lua.org/pil/15.2.html
+
 Some_Namespace = {}
 
 Some_Namespace.some_function = function()
