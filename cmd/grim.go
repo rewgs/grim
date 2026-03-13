339855f59ad46f5b61913efe38299eac472174b5
Alex Ruger <alexrugermusic@gmail.com>
2026-03-13 14:10:54 -0700

    Began basic cli.

--- cmd/grim.go
+++ cmd/grim.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/urfave/cli/v3"
+)
+
+func main() {
+	cmd := &cli.Command{
+		Name:  "grim",
+		Usage: "ReaScript helper tool",
+		Action: func(context.Context, *cli.Command) error {
+			fmt.Println("Testing 1 2 3")
+			return nil
+		},
+	}
+
+	if err := cmd.Run(context.Background(), os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
